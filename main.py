178a2737b1f65e62de0ffc8c3b3a7faf36435396
Sebastian Reinig <sebastianreinig@users.noreply.github.com>
2024-10-19 19:13:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -58,5 +58,5 @@ def upload_file():
     return render_template('upload.html', transcription=transcription_text)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #app.run()
